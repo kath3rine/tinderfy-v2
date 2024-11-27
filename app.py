72691d709ba7370bc91222e7a812a7d0dfc05a93
kath3rine <not.katherine.li@gmail.com>
2024-11-27 04:25:09 -0500

    top artists

--- app.py
+++ app.py
@@ -1,156 +1,54 @@
-from flask import (
-    abort,
-    Flask,
-    make_response,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for,
-)
-import json
-import logging
-import os
-import requests
-import secrets
-import string
+from flask import Flask, session, make_response, redirect, request
+# from util import *
 from urllib.parse import urlencode
+import requests
 from secret import CLIENT_ID, CLIENT_SECRET
 
-
-logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG
-)
-
-
-# Client info
 REDIRECT_URI = "http://127.0.0.1:5000/callback"
 
-
-# Spotify API endpoints
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
-ME_URL = 'https://api.spotify.com/v1/me'
+BASE_URL = 'https://api.spotify.com/v1/'
 
-
-# Start 'er up
 app = Flask(__name__)
 app.secret_key = "secretkey"
 
-
 @app.route('/')
 def login():
-
-    # redirect_uri can be guessed, so let's generate
-    # a random `state` string to prevent csrf forgery.
-    # state = ''.join(
-    #     secrets.choice(string.ascii_uppercase + string.digits) for _ in range(16)
-    # )
-
-    # Request authorization from user
-    scope = 'user-read-private user-read-email'
-
-    
     payload = {
-            'client_id': CLIENT_ID,
-            'response_type': 'code',
-            'redirect_uri': REDIRECT_URI,
-            # 'state': state,
-            'scope': scope,
-        }
-
-    res = make_response(redirect(f'{AUTH_URL}/?{urlencode(payload)}'))
-    # res.set_cookie('spotify_auth_state', state)
-
-    return res
-
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': REDIRECT_URI, 
+        'scope': 'user-top-read user-library-read'
+    }
+    return make_response(redirect(f'{AUTH_URL}/?{urlencode(payload)}'))
 
 @app.route('/callback')
 def callback():
-    error = request.args.get('error')
     code = request.args.get('code')
-    # state = request.args.get('state')
-    # stored_state = request.cookies.get('spotify_auth_state')
-
-    # Check state
-    # if state is None or state != stored_state:
-    #     app.logger.error('Error message: %s', repr(error))
-    #     app.logger.error('State mismatch: %s != %s', stored_state, state)
-    #     abort(400)
-
-    # Request tokens with code we obtained
     payload = {
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': REDIRECT_URI,
     }
 
-    # `auth=(CLIENT_ID, SECRET)` basically wraps an 'Authorization'
-    # header with value:
-    # b'Basic ' + b64encode((CLIENT_ID + ':' + SECRET).encode())
-    res = requests.post(TOKEN_URL, auth=(CLIENT_ID, CLIENT_SECRET), data=payload)
-    res_data = res.json()
-
-    if res_data.get('error') or res.status_code != 200:
-        app.logger.error(
-            'Failed to receive token: %s',
-            res_data.get('error', 'No error information received.'),
-        )
-        abort(res.status_code)
-
-    # Load tokens into session
+    response = requests.post(TOKEN_URL, auth=(CLIENT_ID, CLIENT_SECRET), data=payload)
+    response = response.json()
     session['tokens'] = {
-        'access_token': res_data.get('access_token'),
-        'refresh_token': res_data.get('refresh_token'),
+        'access_token': response.get('access_token'),
+        'refresh_token': response.get('refresh_token'),
     }
 
-    return redirect(url_for('me'))
+    return redirect('/profile')
 
-
-@app.route('/refresh')
-def refresh():
-    '''Refresh access token.'''
-
-    payload = {
-        'grant_type': 'refresh_token',
-        'refresh_token': session.get('tokens').get('refresh_token'),
-    }
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-    res = requests.post(
-        TOKEN_URL, auth=(CLIENT_ID, CLIENT_SECRET), data=payload, headers=headers
-    )
-    res_data = res.json()
-
-    # Load new token into session
-    session['tokens']['access_token'] = res_data.get('access_token')
-
-    return json.dumps(session['tokens'])
-
-
-@app.route('/me')
-def me():
-    '''Get profile info as a API example.'''
-
-    # Check for tokens
-    if 'tokens' not in session:
-        app.logger.error('No tokens in session.')
-        abort(400)
-
-    # Get profile info
+@app.route('/profile')
+def profile():
     headers = {'Authorization': f"Bearer {session['tokens'].get('access_token')}"}
 
-    res = requests.get(ME_URL, headers=headers)
-    res_data = res.json()
-
-    if res.status_code != 200:
-        app.logger.error(
-            'Failed to get profile info: %s',
-            res_data.get('error', 'No error message returned.'),
-        )
-        abort(res.status_code)
-
-    return str(res_data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    top_artists_params = { 'limit': 10 }
+    top_artists = requests.get(f"{BASE_URL}me/top/artists?limit=10", headers=headers)
+    top_artists = top_artists.json()
+    top_artists_names = []
+    for a in top_artists['items']:
+        top_artists_names.append(a['name'])
+    return top_artists_names
